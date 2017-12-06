6b6a955b12d681370f7cb8a5a3d18f6318019085
fpwl <florence.lo@hotmail.com>
2017-12-07 12:55:27 +1300

    update 4

--- js/custom.js
+++ js/custom.js
@@ -77,7 +77,6 @@
         }
     }
 
-
     // Adding and Minus number of seats function
     function addMinusSeats() {
         var addSeat = document.getElementById('addSeat');
@@ -114,36 +113,37 @@
         addDay.addEventListener('click', addDays, false);
         minusDay.addEventListener('click', minusDays, false);
 
-        
-            
-            function addDays() {
-                if (dayCount >= 0 && dayCount <= 14) {
-                    dayCount++;
-                    dayQuantity.value = dayCount;
-                }
-            }
-            function minusDays() {
-                if (dayCount > 0 && dayCount <= 15) {
-                    dayCount--;
-                    dayQuantity.value = dayCount;
-                }
+        function addDays() {
+            if (dayCount >= 0 && dayCount <= 14) {
+                dayCount++;
+                dayQuantity.value = dayCount;
             }
         }
+        function minusDays() {
+            if (dayCount > 0 && dayCount <= 15) {
+                dayCount--;
+                dayQuantity.value = dayCount;
+            }
+        }
+     }
 
+//____________________________________________________________________________________________________//
 
-    //Validating Question One
-    function questionOneValidation () {
+    //Valiadtion JS for the valiadtion of the form questions
+
+    //Validation for Question One
+    function questionOneValidation() {
         //Type of trip option variables
         var customiseButton = document.getElementById('customise');
         var northIsland = document.getElementById('northIsland');
         var southIsland = document.getElementById('southIsland');
         var fullTrip = document.getElementById('fullTrip');
 
-        if (customiseButton.checked){
+        if (customiseButton.checked) {
             document.getElementById('questionOne').style.display = 'none';
             document.getElementById('questionOneCustomise').style.display = 'block';
         }
-        else if (fullTrip.checked || southIsland.checked || northIsland.checked){
+        else if (fullTrip.checked || southIsland.checked || northIsland.checked) {
             document.getElementById('questionOne').style.display = 'none';
             document.getElementById('questionTwo').style.display = 'block';
         }
@@ -153,24 +153,24 @@
     }
 
     //Validating Question One Customise
-    function customiseQuestionValidation () {
+    function customiseQuestionValidation() {
         var startingPointSelect = document.getElementById('sel1');
         var endingPointSelect = document.getElementById('sel2');
 
-        if (startingPointSelect.value && endingPointSelect.value){
+        if (startingPointSelect.value && endingPointSelect.value) {
             document.getElementById('questionOneCustomise').style.display = 'none';
             document.getElementById('questionTwo').style.display = 'block';
         }
-        else{
+        else {
             console.log('choose something please');
         }
     }
 
     //Validating Question Two 
-    function questionTwoValidation () {
+    function questionTwoValidation() {
         var seatQuantity = document.getElementById('seatQuantity');
 
-        if (seatQuantity.value > 0){
+        if (seatQuantity.value > 0) {
             document.getElementById('questionTwo').style.display = 'none';
             document.getElementById('questionThree').style.display = 'block';
         }
@@ -181,7 +181,7 @@
 
     //Validating Question Three 
     //Taking seat and day quantity to determine what vehicles will be displayed in question four
-    function questionThreeValidation () {
+    function questionThreeValidation() {
         var dayQuantity = document.getElementById('dayQuantity').value;
         var seatQuantity = document.getElementById('seatQuantity').value;
 
@@ -196,20 +196,22 @@
         showMotorhome()
         showSmallCar()
 
-        function showMotorbike () {
-            if (seatQuantity == 1 && (dayQuantity > 0 && dayQuantity <= 5)){
+        function showMotorbike() {
+            if (seatQuantity == 1 && (dayQuantity > 0 && dayQuantity <= 5)) {
                 motorbike.style.display = 'block';
                 document.getElementById('questionThree').style.display = 'none';
                 document.getElementById('questionFour').style.display = 'block';
+
+                vehicleModalData()
             }
-            else{
+            else {
                 motorbike.style.display = 'none';
                 console.log('motorbike hidden')
             }
         }
 
-        function showSmallCar () {
-            if ((seatQuantity > 0 && seatQuantity <= 2) && (dayQuantity > 0 && dayQuantity <= 10)){
+        function showSmallCar() {
+            if ((seatQuantity > 0 && seatQuantity <= 2) && (dayQuantity > 0 && dayQuantity <= 10)) {
                 smallCar.style.display = 'block';
                 document.getElementById('questionThree').style.display = 'none';
                 document.getElementById('questionFour').style.display = 'block';
@@ -220,19 +222,19 @@
             }
         }
 
-        function showLargeCar () {
-            if ((seatQuantity > 0 && seatQuantity <= 5) && (dayQuantity >=3 && dayQuantity <= 10)){
+        function showLargeCar() {
+            if ((seatQuantity > 0 && seatQuantity <= 5) && (dayQuantity >= 3 && dayQuantity <= 10)) {
                 largeCar.style.display = 'block';
                 document.getElementById('questionThree').style.display = 'none';
                 document.getElementById('questionFour').style.display = 'block';
             }
-            else{
+            else {
                 largeCar.style.display = 'none';
                 console.log('Large Car hidden')
             }
         }
 
-        function showMotorhome () {
+        function showMotorhome() {
             if ((seatQuantity >= 2 && seatQuantity <= 6) && (dayQuantity >= 2 && dayQuantity <= 15)) {
                 motorhome.style.display = 'block';
                 document.getElementById('questionThree').style.display = 'none';
@@ -245,7 +247,37 @@
         }
     }
 
-    //Map Box js stuff
+//_________________________________________________________________________________________________________//
+
+    //Vehicle option modal data function
+    function vehicleModalData () {
+        var dayQuantity = document.getElementById('dayQuantity').value;
+        var seatQuantity = document.getElementById('seatQuantity').value;
+
+        var startingPointSelect = document.getElementById('sel1');
+        var endingPointSelect = document.getElementById('sel2');
+
+        var northIsland = document.getElementById('northIsland');
+        var southIsland = document.getElementById('southIsland');
+        var fullTrip = document.getElementById('fullTrip');
+
+        var motorbike = document.getElementById('motorbike');
+        var smallCar = document.getElementById('smallCar');
+        var largeCar = document.getElementById('largeCar');
+        var motorhome = document.getElementById('motorhome');
+
+        motorbike.addEventListener('click', motorbikeModalData, false);
+
+        // function motorbikeModalData () {
+            
+        // }
+    }
+
+//__________________________________________________________________________________________________________//
+
+    //custom js for mapbox styles
+
+    //Displaying Mapbox map
     function mapbox() {
         mapboxgl.accessToken = 'pk.eyJ1IjoiZnB3bCIsImEiOiJjamFib2swNnMwMjU0MndwZGwzdnhmZXBnIn0.0glfG4jZkFJwm0TJLuVriA';
 
@@ -253,12 +285,131 @@
             container: 'map', // container id
             style: 'mapbox://styles/fpwl/cjan5kfxmeaq02smsehvd41aa', // stylesheet location
             center: [174.8860, -40.9006], // starting position [lng, lat]
-            zoom: 4.5 // starting zoom 
+            zoom: 4 // starting zoom 
         });
+
+        
+
+        document.getElementById('northIsland').addEventListener('click', showMarkers, false);
+        document.getElementById('southIsland').addEventListener('click', showMarkers, false);
+        document.getElementById('fullTrip').addEventListener('click', showMarkers, false);
+
+
+
+        function showMarkers () {
+            if (document.getElementById('fullTrip').checked) {
+                northIslandData.features.forEach(function (marker) {
+                    // create a HTML element for each feature
+                    var el = document.createElement('div');
+                    el.className = 'marker';
+
+                    // make a marker for each feature and add to the map
+                    new mapboxgl.Marker(el)
+                        .setLngLat(marker.geometry.coordinates)
+                        .addTo(map);
+                });
+                southIslandData.features.forEach(function (marker) {
+                    // create a HTML element for each feature
+                    var el = document.createElement('div');
+                    el.className = 'marker';
+
+                    // make a marker for each feature and add to the map
+                    new mapboxgl.Marker(el)
+                        .setLngLat(marker.geometry.coordinates)
+                        .addTo(map);
+                });
+            }
+            else if (document.getElementById('northIsland').checked) {
+                northIslandData.features.forEach(function (marker) {
+                    // create a HTML element for each feature
+                    var el = document.createElement('div');
+                    el.className = 'marker';
+
+                    // make a marker for each feature and add to the map
+                    new mapboxgl.Marker(el)
+                        .setLngLat(marker.geometry.coordinates)
+                        .addTo(map);
+                });
+            }
+            else if (document.getElementById('southIsland').checked) {
+                southIslandData.features.forEach(function (marker) {
+                    // create a HTML element for each feature
+                    var el = document.createElement('div');
+                    el.className = 'marker';
+
+                    // make a marker for each feature and add to the map
+                    new mapboxgl.Marker(el)
+                        .setLngLat(marker.geometry.coordinates)
+                        .addTo(map);
+                });
+            }
+            else if (document.getElementById('customise').checked) {
+                document.getElementById('nextArrowOne').style.color = '#ffdd12';
+            }
+
+        }
     }
+        
+        // document.getElementById('northIsland').addEventListener('click', northHalfMarkers, false);
+        // document.getElementById('southIsland').addEventListener('click', southHalfMarkers, false);
+        // document.getElementById('fullTrip').addEventListener('click', fullTripMarkers, false);
+
+        //functions to add mapbox markers to map
+    //     function fullTripMarkers() {
+    //         // add markers to map
+    //         northIslandData.features.forEach(function (marker) {
+    //             // create a HTML element for each feature
+    //             var el = document.createElement('div');
+    //             el.className = 'marker';
+
+    //             // make a marker for each feature and add to the map
+    //             new mapboxgl.Marker(el)
+    //                 .setLngLat(marker.geometry.coordinates)
+    //                 .addTo(map);
+    //         });
+    //         southIslandData.features.forEach(function (marker) {
+    //             // create a HTML element for each feature
+    //             var el = document.createElement('div');
+    //             el.className = 'marker';
+
+    //             // make a marker for each feature and add to the map
+    //             new mapboxgl.Marker(el)
+    //                 .setLngLat(marker.geometry.coordinates)
+    //                 .addTo(map);
+    //         });
+    //     }
+
+    //     function northHalfMarkers() {
+    //         // add markers to map
+    //         northIslandData.features.forEach(function (marker) {
+    //             // create a HTML element for each feature
+    //             var el = document.createElement('div');
+    //             el.className = 'marker';
+
+    //             // make a marker for each feature and add to the map
+    //             new mapboxgl.Marker(el)
+    //                 .setLngLat(marker.geometry.coordinates)
+    //                 .addTo(map);
+    //         });
+    //     }
+
+    //     function southHalfMarkers() {
+    //         // add markers to map
+    //         southIslandData.features.forEach(function (marker) {
+    //             // create a HTML element for each feature
+    //             var el = document.createElement('div');
+    //             el.className = 'marker';
+
+    //             // make a marker for each feature and add to the map
+    //             new mapboxgl.Marker(el)
+    //                 .setLngLat(marker.geometry.coordinates)
+    //                 .addTo(map);
+    //         });
+    //     }
+    // }
+
 
 })();
-
 
 
 
---
+++ js/tripData.js
@@ -0,0 +1,119 @@
+//North Island Coordinates
+var northIslandData = {
+    type: 'FeatureCollection',
+    features: [{
+        type: 'Feature',
+        geometry: {
+        type: 'Point',
+        coordinates: [173.2676, -35.1173]
+        },
+        properties: {
+        title: 'Mapbox',
+        description: 'Kaitaia, New Zealand'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+        type: 'Point',
+        coordinates: [174.7633, -36.8485]
+        },
+        properties: {
+        title: 'Mapbox',
+        description: 'Auckland, New Zealand'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [175.1894, -37.4558]
+        },
+        properties: {
+            title: 'Mapbox',
+            description: 'Waikato, New Zealand'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [175.0479, -39.9301]
+        },
+        properties: {
+            title: 'Mapbox',
+            description: 'Whanganui, New Zealand'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [174.7762, -41.2865]
+        },
+        properties: {
+            title: 'Mapbox',
+            description: 'Wellington, New Zealand'
+        }
+    }]
+};
+
+//South Island Coordinates
+var southIslandData = {
+    type: 'FeatureCollection',
+    features: [{
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [171.2108, -42.4504]
+        },
+        properties: {
+            title: 'Mapbox',
+            description: 'Greymouth, New Zealand'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [172.6362, -43.5321]
+        },
+        properties: {
+            title: 'Mapbox',
+            description: 'Christchurch, New Zealand'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [168.6626, -45.0312]
+        },
+        properties: {
+            title: 'Mapbox',
+            description: 'Queenstown, New Zealand'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [170.5028, -45.8788]
+        },
+        properties: {
+            title: 'Mapbox',
+            description: 'Dunedin, New Zealand'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [168.3538, -46.4132]
+        },
+        properties: {
+            title: 'Mapbox',
+            description: 'Invercargill, New Zealand'
+        }
+    }]
+};
